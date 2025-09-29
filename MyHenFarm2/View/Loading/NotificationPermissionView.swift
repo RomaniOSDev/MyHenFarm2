d8849198461b90a3182587c4ec1c6ffb4a364460
Roman <romazan.26@gmail.com>
2025-09-29 11:37:13 +0300

    f

--- MyHenFarm2/View/Loading/NotificationPermissionView.swift
+++ MyHenFarm2/View/Loading/NotificationPermissionView.swift
@@ -213,8 +213,7 @@ struct NotificationPermissionView: View {
     
     private func presentWebView(from viewController: UIViewController) {
         print("🚀 Creating WebView with URL: \(webURL)")
-        //let webviewVC = WebviewVC(url: webURL)
-        let webviewVC = CoreViewController(url: webURL)
+        let webviewVC = WebviewVC(url: webURL)
         webviewVC.modalPresentationStyle = .fullScreen
         
         print("📱 Presenting WebView from: \(type(of: viewController))")
